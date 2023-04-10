3fd0f0c0d10c88e188ea122e21e476e140289891
Brandon Kaplan <59454456+BrandonKaplan@users.noreply.github.com>
2023-04-10 00:22:45 -0700

    Created artificial recording generator

---
+++ 483_Term_Project/artificial_data_generator.py
@@ -0,0 +1,203 @@
+'''
+AUTHOR: Hazel (Brandon) Kaplan
+CLASS: CSC 483 - Text Retrieval and Web Search
+ASSIGNMENT: Term Project
+DESCRIPTION: Creates artificial pronunciations of words for a single set of phoneme recordings.
+These artificial recordings are stored in a folder that is stores all the words in a wav audio
+format. This program first takes in a name of the file that has a list of all the phonemes that
+the program is using to generate the pronunciations, it then takes a name of the subject (folder
+name) that contains all the recordings of the phonemes. Then it takes in one last additional file
+which contains all the pronunciations in a text format that will have an artificial recording
+be generated from
+
+FILE SYSTEM STRUCTURE:
+    483_Term_Project:
+        This program
+        phoneme_list.txt
+        phonetic_pronunciations.txt
+        Recordings:
+            Subject One
+                phoneme_one.wav
+                phoneme_two.wav
+                ...
+            Subject Two
+            Subject Three
+            ...
+        Artificial Data:
+            Subject One
+                word_one.wav
+                word_two.wav
+                ...
+            Subject Two
+            Subject Three
+            ...
+        ...
+        
+
+INPUT FILE FORMAT:
+
+    PHONEME FILE:
+    The file with the used phonemes should have one phoneme per line and use all caps:
+    AE
+    K
+    IY
+    IH
+    ...
+
+    PRONUNCIATION FILE:
+    The file used for the pronunciations should be formatted as follows:
+    BEEF	B IY F
+    CUTE	K Y UW T
+    TIRED	T AY ER D
+    DEPRESSED	D IH P R EH S T
+    MOTIVATION	M OW T AH V EY SH AH N
+    APPLE	AE P AH L
+    SAD	S AE D
+    PERSEVERE	P ER S AH V IH R
+    WORK	W ER K
+
+
+DEVELOPER TECHNICAL DOCUMENTATION:
+- The phoneme is assumed to be in the same directory as this program is located in
+- "Subject" refers to the person that the program is contructing the artificial data for
+- Must have the PyDub library installed for this program to work, run "pip install pydub"
+  in the terminal
+  
+'''
+
+
+# imports the audio library
+from pydub import AudioSegment, silence
+import numpy as np
+
+'''
+SUMMARY: Creates a single artificial pronunciation of a word
+
+DESCRIPTION: Given a pronunciation of a word, this function creates an artificial pronunciation
+by adding the segments of audio to build the full pronunciation from the pronunciation
+dictionary
+
+PARAMETERS:
+    - phonetic_pronunciation: Python list of phonemes which are strings
+    - pronunciation_dictionary: dictionary that maps phoneme strings to the AudioSegment
+    pronunciation from the subject
+
+'''
+def create_artificial_word(phonetic_pronunciation, pronunciation_dictionary):
+    # creates an empty AudioSegment to add phonemes to
+    artificial_word = AudioSegment.empty()
+    # iterates through the pronunciations
+    for phoneme in phonetic_pronunciation:
+        # looks up the AudioSegment in the pronunciation_dictionary
+        phoneme_audio = pronunciation_dictionary[phoneme]
+        # adds/builds the AudioSegment by adding the audio
+        artificial_word += phoneme_audio
+    return artificial_word
+    
+
+'''
+SUMMARY: Creates the artificial pronunciationa of each of the words
+
+DESCRIPTION: Opens the file with the pronunciations for the various words, goes through them
+all, then creates an artificial pronunciation of the word using the phoneme dictionary for the
+subject. This is all written in one function but uses other functions to prevent having to loop
+through the dictionary twice O(N^2) if it were to just get the pronunciation content first. This
+function then exports the artifiical data created to an Artificial Recordings folder with the
+subject's name and store every word there. 
+
+PARAMETERS:
+    - pronunciation dictionary: A dictionary that maps the phoneme string to the AudioSegment
+    type recording of the subjects phoneme
+    - subject_name: String that is the name of the directory of the subjects recordings
+'''
+def create_artificial_data(subject_name, pronunciation_dictionary):
+    # constants for this function
+    WORD_INDEX = 0
+    PHONEME_INDEX = 1
+    
+    # gets the name and opens the file with the words with their phonetic pronunciations
+    word_file_name = input("Enter name of the file that contains the words and their phonetic" \
+                    + "pronunciations: \n")
+    word_file = open(word_file_name, "r")
+
+    # iterates for all the words and pronunciations in the file
+    for word_and_pronunciation in word_file:
+        split_line = word_and_pronunciation.split()
+        # word that the program is constructing artifical data for
+        word = split_line[WORD_INDEX]
+        # the pronunciation text version for the word
+        phonetic_pronunciation = split_line[PHONEME_INDEX:]
+        # gets the artificial pronunciation AudioSegment of the word
+        artificial_pronunciation = create_artificial_word(phonetic_pronunciation, pronunciation_dictionary)
+        # creates the path exports the artificial file to the directory
+        artificial_storage_path =  "Artificial Pronunciations/" + subject_name + "/" + word + ".wav"
+        artificial_pronunciation.export(artificial_storage_path, format="wav")
+    return 
+
+
+'''
+SUMMARY: Creates a pronunciation dictionary of the subject's phonemes
+
+DESCRIPTION: Using a list of phonemes, this function creates a pronunciation dictionary by
+mapping the string phoneme to an AudioSegment that contains the subject's pronunciation for
+that phoneme
+
+PARAMETERS:
+    - subject_name: String that is the name of the directory of the subjects recordings
+    - phoneme_list: Python list of the phonemes in all caps
+    
+RETURNS:
+    - pronunciation_dictionary: Dictionary containing the phonemes as keys and the AudioSegment
+    of the subjects pronunciation of that phoneme
+'''
+def get_pronunciation_dictionary(subject_name, phoneme_list):
+    pronunciation_dictionary = {}
+
+    # goes through each of the phoneme_list
+    for phoneme in phoneme_list:
+        # contructs the file path for the subject
+        pronunciation_file_path = "Recordings/" + subject_name + "/" + phoneme + ".wav"
+        # gets the pronunciation as the form of an AudioSegment
+        phonetic_pronunciation = AudioSegment.from_file(pronunciation_file_path, format='wav')
+        # maps the phoneme to the AudioSegment pronunciation
+        pronunciation_dictionary[phoneme] = phonetic_pronunciation
+
+    return pronunciation_dictionary
+
+
+'''
+SUMMARY: Reads in a list of phonemes that will be used that are in the pronunciations
+
+DESCRIPTION: Takes in a file name through input and then reads the entire list of phonemes
+that are stated in the file. This also strips off the newline characters for easy matching
+in the future when contructing a pronunciation dictionary
+
+RETURNS:
+    - cleaned_list: Python list that contains the phonemes as strings 
+'''
+def get_phoneme_list():
+    # gets the name of the file, opens it, and reads the contents in
+    phoneme_file_name = input("Enter file containing phonemenes: \n")
+    phoneme_file = open(phoneme_file_name, "r")
+    phonemes = phoneme_file.readlines()
+
+    # closes the file since we extracted all the contents out of it
+    phoneme_file.close()
+    
+    # goes through each of the phoneme and strips/"cleans" off the newline
+    cleaned_list = []
+    for phoneme in phonemes:
+        phoneme_cleaned = phoneme.rstrip("\n")
+        cleaned_list.append(phoneme_cleaned)
+        
+    return cleaned_list
+
+
+def main():
+    phoneme_list = get_phoneme_list()
+    subject_name = input("Enter the name of the person's recordings (Folder Name): \n")
+    pronunciation_dictionary = get_pronunciation_dictionary(subject_name, phoneme_list)
+    create_artificial_data(subject_name, pronunciation_dictionary)
+    
+
+main()
